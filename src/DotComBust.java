bfb2a850bf6babaed015f57d4d63a1598a6022c9
Zhigunenko N.A <icpo@ya.ru>
2015-09-03 08:10:39 +0300

    [win] init

---
+++ src/DotComBust.java
@@ -0,0 +1,71 @@
+/**
+ * Created by Username on 14.08.15.
+ */
+import java.util.*;
+
+public class DotComBust {
+    private GameHelper helper = new GameHelper();
+    private ArrayList<DotCom> dotComList = new ArrayList<DotCom>();
+    private int numOfGuesses = 0;
+
+    private void setUpGame(){
+        DotCom one = new DotCom();
+        one.setName("Pets.com");
+        DotCom two = new DotCom();
+        two.setName("eToys.com");
+        DotCom three = new DotCom();
+        three.setName("Go2.com");
+        dotComList.add(one);
+        dotComList.add(two);
+        dotComList.add(three);
+
+        System.out.println("Убей три сайта");
+        System.out.println("pets, eToys, Go2");
+        System.out.println("Пробуй");
+
+        for (DotCom dotComToSet : dotComList) {
+            ArrayList<String> newLocation = helper.placeDotCom(3);
+            dotComToSet.setLocationCells(newLocation);
+        }
+    }
+
+    private void startPlaying(){
+        while (!dotComList.isEmpty()){
+            String userGuess = helper.getUserInput("Делай ход");
+            checkUserGuess(userGuess);
+        }
+        finishGame();
+    }
+
+    private void checkUserGuess(String userGuess) {
+        numOfGuesses++;
+        String result = "Мимо";
+
+        for (DotCom dotComToTest : dotComList){
+            result = dotComToTest.checkYourself(userGuess);
+            if (result.equals("Попал")) {
+                break;
+            }
+            if (result.equals("Потопил")) {
+                dotComList.remove(dotComToTest);
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+
+    private void finishGame() {
+        System.out.println("Все сайты ушли ко дну");
+        if (numOfGuesses <= 18) {
+            System.out.println("Вы выбрались, это заняло у вас " + numOfGuesses + " попыток");
+        } else {
+            System.out.println("вы слишком долго боролись, это заняло у вас " + numOfGuesses + " попыток");
+        }
+    }
+
+    public static void main (String[] args) {
+        DotComBust game = new DotComBust();
+        game.setUpGame();
+        game.startPlaying();
+    }
+}
